7ef83c9c6663554978ed932edfd9a0357ad1a64e
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-07-24 10:16:15 -0700

    added questions array and the readme template literal in generatemarkdown.js

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 // TODO: Include packages needed for this application
+const genMd = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project title?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "What is your project description"
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: "Installation instructions"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "How do you use this app?"
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: "Who contributed?"
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "Tests"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email"
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your username?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "What license do you wish to choose?",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'none'],
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(readMe, data) {
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,50 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
+// we put our readme inside this template literal ex. => data.description, data.usage, data.
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
 
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Test](#test)
+  * [Contact](#contact)
+  * [License](#license)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Test 
+  ${data.test}
+
+  ## Contact
+  - Email: ${data.email}
+  - Github: https://github.com/${data.username}
+
+  ## License 
+  ### ${data.license}
+  ${renderLicenseSection(data.license)}
 `;
 }
 
